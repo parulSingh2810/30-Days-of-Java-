28a04169f7885b15a33b5015bb9287d68b7eb4eb
Parul Singh <74547379+parulSingh2810@users.noreply.github.com>
2024-10-13 11:10:47 +0530

    Add files via upload

---
+++ JavaDay1/src/com/accenture/SwitchDemo.java
@@ -0,0 +1,29 @@
+package com.accenture;
+
+import java.util.Scanner;
+
+public class SwitchDemo {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter 1 for Movies");
+	System.out.println("Enter 2 for Shopping");
+	System.out.println("Enter 3 for Eating Out");
+	System.out.println("Enter 4 for No Plan");
+	int x = sc.nextInt();
+	switch(x) {
+	case 1:
+		System.out.println("Going for movies");
+		break;
+	case 2:
+		System.out.println("Going for shopping");
+		break;
+	case 3:
+		System.out.println("Going for Eating Out");
+		break;
+	case 4:
+		System.out.println("No Plan");
+		break;
+	}
+	
+	}
+}
